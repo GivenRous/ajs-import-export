7ab76d7158cdc81d2e257899262908f714c6d675
Ivan Surovegin <givenrous@yandex.ru>
2024-03-15 11:30:21 +0300

    правки

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Game, { GameSavingData, readGameSaving as loadGame, readGameSaving as saveGame } from './game';
+import Game, { GameSavingData, readGameSaving as loadGame, readGameSaving as saveGame } from './game.js';
 
 const game = new Game();
 game.start();
